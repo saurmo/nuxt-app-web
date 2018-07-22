fabca40935c67f2ab180551fb5bffd1cd0cf5f4e
Santiago Urrego Morales <saurmo0108@gmail.com>
2018-07-21 23:12:21 -0500

    Create project template nuxt app web

---
+++ README.md
@@ -0,0 +1,52 @@
+# APP WEB
+
+>NUXT APP WEB is a template  nuxt with  vuetify, axios, Vue moment, vue filters and Vue cookie 
+
+## DEPENDENCIES
+0. Vuetify            https://vuetifyjs.com/en/                        npm install vuetify --save
+1. Vue Filters        https://github.com/freearhey/vue2-filters        npm install vue2-filters --save 
+2. Vue moment         https://github.com/brockpetrie/vue-moment        npm install vue-moment --save
+3. Vue cookie         https://github.com/alfhen/vue-cookie             npm install vue-cookie --save
+
+
+## How do I start it?
+
+> From https://github.com/nuxt-community/create-nuxt-app
+
+``` bash
+0. Prerequisites NPM since 5.2.0
+1. $ npm install -g npx
+2. $ npx create-nuxt-app
+3. Select serve none(nuxt)
+4. Select UI framework (Vuetify)
+5. Rendering mode (Universal)
+6. Axios module (yes)
+7. eslint (no)
+8. package manager (npm)
+
+```
+
+
+
+## Build Setup from nuxt
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
+
+```
+
+
+
---
+++ nuxt.config.js
@@ -0,0 +1,86 @@
+const pkg = require('./package')
+
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#FFFFFF' },
+
+
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/vuetify',
+    '@/plugins/vue2-filters',
+    '@/plugins/vue-moment',
+    '@/plugins/vue-cookie'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+
+      if (ctx.isServer) {
+        config.externals = [
+          nodeExternals({
+            whitelist: [/^vuetify/]
+          })
+        ]
+      }
+    }
+  },
+  /*
+** Global CSS
+*/
+  css: [
+    '@/node_modules/vuetify/dist/vuetify.min.css'
+    // 'vuetify/dist/vuetify.min.css'
+  ],
+  router: {
+    // base: '/app-web/'
+    base: '/'
+  },
+  generate: {
+    dir: 'app-web'
+  },
+}
